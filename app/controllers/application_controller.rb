8a67b572957af517af00ebaa213c94d009231802
Jennifer Gifford <jgifford82@gmail.com>
2022-11-07 20:46:24 -0700

    commit tree 2d4e33fffd7c306514c933d343f7bbb88f30b8e7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,43 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/' do
+    "Hello World"
+  end
+
+  get '/bakeries' do
+    "Hello World"
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+  # GET /bakeries/:id returns a single bakery as JSON with its baked goods nested
+    #get a single bakery from the database
+    bakery = Bakery.find(params[:id])
+    #send back as JSON with baked goods in nested array
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+  # GET /baked_goods/by_price returns an array of baked goods as JSON, sorted by price in descending order
+      #get baked goods from the database
+      baked_goods = BakedGood.all.order(:price).reverse_order
+      #send back as JSON sorted by price in descending order
+      baked_goods.to_json
+  end
+    
+  get '/baked_goods/most_expensive' do
+    # GET /baked_goods/most_expensive returns the single most expensive baked good as JSON (HINT: how can you sort the baked goods in a particular order?)
+      #get baked goods from the database
+      baked_goods = BakedGood.order(:price).reverse_order.first!
+      #send back as JSON sorted by price in descending order
+      baked_goods.to_json
+  end
+      
 
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,9 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+
+  # def sort_goods
+      #run BakedGood.all.sort in rake console to see what it returns
+  #   BakedGood.all.sort
+  # end
 end
